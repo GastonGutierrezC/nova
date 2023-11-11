2ca37fb404cd63f7652585540efb9e324b1b7cbe
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-11-11 17:54:17 -0400

    All information for the

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Import connection
+const dbFile = require('./connection');
+
+// Import routes and user model
+const userRoute = require('./routes/user');
+const productRoute = require('./routes/product');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: 'true' }));
+
+app.use('/api/user', userRoute);
+app.use('/api/products', productRoute);
+
+app.get('/', (req, res) => {
+    res.end('Welcome to the backend server');
+});
+
+app.listen(PORT, function () {
+    console.log("The NODE server is running correctly");
+});
