a874d9fe8daa2cef9f1977bb16f7957bd953f66d
Gaston Gutierrez <gaston.gutierrez.9964359@usalesiana.edu.bo>
2023-11-12 17:19:27 -0400

    All kdszxsdcxlkfgsdsddssddfcvdfdfdfd

--- my-pet-shop/src/components/ProductAllInformation.js
+++ my-pet-shop/src/components/ProductAllInformation.js
@@ -20,22 +20,27 @@ function ProductAllInformation() {
     <div>
       <h2>Food Information</h2>
       <div className="testimonials-container">
-        {products.map((product) => (
-          <Testimonial
-            key={product.productId }
-            name={product.name}
-            position={`Price: ${product.price}`}
-           // testimony={product.description}
-            image={"Foo2"} // Pasa la propiedad de la imagen
-            type={product.type} // Pasa la propiedad del tipo
-            species={product.species} // Pasa la propiedad de la especie
-            quantity={product.quantity} // Pasa la propiedad de la cantidad
-          />
-        ))}
+        {Array.isArray(products) && products.length > 0 ? (
+          products.map((product) => (
+            <Testimonial
+              key={product.productId}
+              name={product.name}
+              position={`Price: ${product.price}`}
+              // testimony={product.description}
+              image={"Foo2"} // Pasa la propiedad de la imagen
+              type={product.type} // Pasa la propiedad del tipo
+              species={product.species} // Pasa la propiedad de la especie
+              quantity={product.quantity} // Pasa la propiedad de la cantidad
+            />
+          ))
+        ) : (
+          <p>No products available</p>
+        )}
       </div>
     </div>
   );
 }
 
 export default ProductAllInformation;
+
 
